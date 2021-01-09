06e424cdf49891a3ee31dbab571f50aac9bdbafa
Aryan Aman <aryanaman97@gmail.com>
2021-01-09 23:02:58 +0530

    validation added

--- src/app/reg-after/reg-after.component.ts
+++ src/app/reg-after/reg-after.component.ts
@@ -13,6 +13,10 @@ export class RegAfterComponent implements OnInit {
   userData={};
   hide = true;
   unique=""
+  user:any=[];
+  Uname=false;
+  Gmail=false;
+  phn=false;
 
   constructor(private fb:FormBuilder, private route:Router, private _snack:MatSnackBar, private service:MainService) { 
     this.form=this.fb.group({
@@ -32,7 +36,23 @@ export class RegAfterComponent implements OnInit {
 
 
   register(){
+    this.service.getUserData().subscribe(data=>{
+      this.user=data;
 
+      for(let i=0;i<this.user.length;i++){
+
+        if(this.form.value.username===this.user[i].username){
+             this.Uname=true;
+
+        }
+
+        if(this.form.value.email===this.user[i].email){
+          this.Gmail=true;
+        }
+        if(this.form.value.tel===this.user[i].tel){
+          this.phn=true;
+        }
+      }
     if(this.form.value.password!=this.form.value.cpassword){
       this._snack.open("password and confirm password not matched", "cancel", {
         duration: 2000,
@@ -45,6 +65,29 @@ export class RegAfterComponent implements OnInit {
       this._snack.open("please fill all the data", "cancel", {
         duration: 2000,
       });
+    }
+    else if(this.phn===true){
+      this._snack.open("Mobile number already exist", "cancel", {
+        duration: 2000,
+      });
+      this.phn=false;
+
+    }
+    else if(this.Uname===true){
+      this._snack.open("Username already exist", "cancel", {
+        duration: 2000,
+      });
+
+      this.Uname=false
+
+
+    }
+    else if(this.Gmail===true){
+      this._snack.open("Email already exist", "cancel", {
+        duration: 2000,
+      });
+      this.Gmail=false;
+
     }
 
     else{
@@ -70,11 +113,14 @@ export class RegAfterComponent implements OnInit {
         duration: 2000,
       });
       
-    })
+    }) 
+
 
     return this.route.navigate(['/register'])
-    console.log(this.form.value);
+    //console.log(this.form.value);
     }
+
+  })
 
   }
 
